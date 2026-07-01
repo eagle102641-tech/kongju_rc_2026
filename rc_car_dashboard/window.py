f8fcd48425f4a0a4918d1838a48ea857ea68f023
eagle102641-tech <eagle102641@gmail.com>
2026-07-01 16:06:14 +0900

    dd

--- rc_car_dashboard/window.py
+++ rc_car_dashboard/window.py
@@ -1,16 +1,19 @@
 import threading
+import time
 import webview
 
 from app import app
 
 
 def run_flask():
-    app.run(port=5000)
+    app.run(host="127.0.0.1", port=5000, debug=False, use_reloader=False)
 
 
 if __name__ == "__main__":
-
     threading.Thread(target=run_flask, daemon=True).start()
+
+    # Flask 서버가 시작될 시간을 조금 준다.
+    time.sleep(2)
 
     webview.create_window(
         "RC CAR Dashboard",
@@ -19,4 +22,4 @@ if __name__ == "__main__":
         height=800
     )
 
-    webview.start()
+    webview.start(gui="qt", debug=True)
