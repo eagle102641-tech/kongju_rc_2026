b508c66ad4386ae368afa117897be4b5804376c7
eagle102641-tech <eagle102641@gmail.com>
2026-07-01 16:15:28 +0900

    dd

--- rc_car_dashboard/static/js/app.js
+++ rc_car_dashboard/static/js/app.js
@@ -1,6 +1,29 @@
-document.getElementById("startBtn")
-.addEventListener("click",()=>{
+function addLog(text){
 
-alert("Dashboard Ready!");
+    const box=document.getElementById("logBox");
 
-});
+    const time=new Date().toLocaleTimeString();
+
+    box.innerHTML+="<br>"+time+" - "+text;
+
+    box.scrollTop=box.scrollHeight;
+
+}
+
+
+function sendCommand(command){
+
+    addLog(command);
+
+}
+
+
+const slider=document.getElementById("speedSlider");
+
+slider.oninput=function(){
+
+    document.getElementById("speedText").innerHTML=this.value+"%";
+
+    addLog("Speed : "+this.value);
+
+}
